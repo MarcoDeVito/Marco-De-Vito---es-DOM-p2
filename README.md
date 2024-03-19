d5055ddefcc0690f2408b83ca9c56912da605c0e
Marco De Vito <marco.devito93@gmail.com>
2024-03-19 18:55:11 +0100

    svolta traccia 3

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 - Array di colori:
 `let colors = ['red', 'black', 'green', 'blue', 'yellow'];`
 
+
+**Traccia 3:**
+Avremo due elementi HTML:
+- un pulsante che ci consentira' di ordinare i contatti
+- un contenitore, in cui verranno visualizzati i contatti
+`<button id="btnAsc">Ordina in modo crescente</button>
+<div class="wrapper"></div>`
+
+Ordinare in ordine alfabetico un elenco di contatti:
+- al primo click del pulsante, verranno ordinati dalla A alla Z
+- al secondo click del pulsante, verranno ordinati dalla Z alla A
+- dato un'array di partenza:
+`contacts = [
+    { 'id': 1, 'name': 'Nicola'},
+    { 'id': 2, 'name': 'Fabio'},
+    { 'id': 3, 'name': 'Luca'},
+    { 'id': 4, 'name': 'Giulia'}
+] `
+
+*EXTRA:*
+- Inserire un secondo pulsante che permetta di ordinarli in base all'id del contatto
+- Inserire un terzo pulsante che permetta di nascondere o visualizzare tutta la lista dei contatti
--- script.js
+++ script.js
@@ -1,17 +1,102 @@
 let counterColor = 0;
-let btn1= document.querySelector('#btn1');
-let h1= document.querySelector('h1');
-let colors = [ 'red','black', 'green', 'blue', 'yellow'];
+let btn1 = document.querySelector('#btn1');
+let h1 = document.querySelector('h1');
+let colors = ['red', 'black', 'green', 'blue', 'yellow'];
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let hide = document.querySelector('#hide');
+let wrapper = document.querySelector('.wrapper');
+let contacts = [
+    { 'id': 1, 'name': 'Nicola' },
+    { 'id': 2, 'name': 'Fabio' },
+    { 'id': 3, 'name': 'Luca' },
+    { 'id': 4, 'name': 'Giulia' }]
+let isDesc = true;
+let isDescID = true;
 
 
 btn1.addEventListener('click', () => {
     h1.style.color = colors[counterColor];
-    console.log(colors[counterColor]);
-    
-    if (counterColor < colors.length - 1) {        
-        counterColor++;        
+    // console.log(colors[counterColor]);
+
+    if (counterColor < colors.length - 1) {
+        counterColor++;
     } else {
         counterColor = 0;
     }
 
+})
+
+btn2.addEventListener('click', () => {
+
+    wrapper.innerHTML="";
+    if (isDesc) {
+        contacts.sort((a, b) => {
+            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+
+            // names must be equal
+            return 0;
+        });
+        console.log(contacts);
+        isDesc = false;
+        btn2.innerHTML = "Ordine alfabetico ⬇️"
+    } else {
+        contacts.sort((a, b) => {
+            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return 1;
+            }
+            if (nameA > nameB) {
+                return -1;
+            }
+
+            // names must be equal
+            return 0;
+        });
+        console.log(contacts);
+        isDesc = true;
+        btn2.innerHTML = "Ordine alfabetico ⬆️"
+    }
+    let article = document.createElement("article");
+    contacts.forEach(contact => {
+        article.innerHTML += `<p>${contact.name}</p>`;
+    });
+    wrapper.appendChild(article);
+    // console.log(article);
+
+})
+
+btn3.addEventListener('click', () => {
+
+    wrapper.innerHTML="";
+    if (isDescID) {
+        contacts.sort((a, b) => a.id - b.id);
+        // console.log(contacts);
+        isDescID = false;
+        btn3.innerHTML = "Ordine di ID ⬇️"
+    } else {
+        contacts.sort((a, b) =>  b.id - a.id  );
+        // console.log(contacts);
+        isDescID = true;
+        btn3.innerHTML = "Ordine di ID ⬆️"
+    }
+    let article = document.createElement("article");
+    contacts.forEach(contact => {
+        article.innerHTML += `<p>${contact.id}: ${contact.name}</p>`;
+    });
+    wrapper.appendChild(article);
+    // console.log(article);
+
+})
+
+hide.addEventListener('click', ()=>{
+    wrapper.classList.toggle("d-none")
 })
